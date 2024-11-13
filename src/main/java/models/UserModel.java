854e0d318abe115d3739f03371d46ad90365bfa4
Hacker Insideit <hacker.insideit@gmail.com>
2024-11-13 10:43:26 -0300

    Update UserModel.java

--- src/main/java/models/UserModel.java
+++ src/main/java/models/UserModel.java
@@ -1,7 +1,6 @@
 package models;
 
 import org.springframework.data.mongodb.core.mapping.Document;
-
 import com.fasterxml.jackson.annotation.JsonProperty;
 
 @Document("UserModel")
@@ -11,30 +10,6 @@ public class UserModel {
 	private String authUserKey;
 	
 	@JsonProperty("email")
-	private String email;	
-		
-	public UserModel(String email, String authUserKey) {
-		super();		
-		this.email = email;
-		this.authUserKey = authUserKey;
-	}		
-	
-	public String toString() {
-		return "UserModel [email=" + email + ", authUserKey=" + authUserKey + "]";
-	}	
-	
-	
-	public String getEmail() {
-		return email;
-	}
-	public void setEmail(String email) {
-		this.email = email;
-	}
-	public String getAuthUserKey() {
-		return authUserKey;
-	}
-	public void setAuthUserKey(String authUserKey) {
-		this.authUserKey = authUserKey;
-	}
+	private String email;		
 	
 }
